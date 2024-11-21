bc974ac9661d82fc37d91709f7b120fb61b9c140
Juan Ramirez <ramirezjuanfreddy8@gmail.com>
2024-11-21 10:48:25 -0400

    edit database url

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres@localhost/postgres',
+        default='postgresql://postgres:postgres@localhost/postgres',
         conn_max_age=600
     )
 }
