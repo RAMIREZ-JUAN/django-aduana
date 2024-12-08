dba8a4239ec367218ac8d017a0a710a1279a34cf
Juan Ramirez <ramirezjuanfreddy8@gmail.com>
2024-12-07 22:15:47 -0400

    backend_changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,7 +81,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
+    'default': 
+    dj_database_url.config(
         default='postgresql://postgres:postgres@localhost/postgres',
         conn_max_age=600
     )
