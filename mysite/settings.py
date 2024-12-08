f57f20dc42c84771f5c656af1b310fe84c5cdae6
Juan Ramirez <ramirezjuanfreddy8@gmail.com>
2024-12-07 20:36:29 -0400

    setting2

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
@@ -85,6 +85,10 @@ DATABASES = {
         default='postgresql://postgres:postgres@localhost/postgres',
         conn_max_age=600
     )
+    #{
+    #'ENGINE':'django.db.backends.sqlite3',
+    #'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 }
 
 
